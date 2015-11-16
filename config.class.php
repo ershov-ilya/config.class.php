abe61413d2c7cae053d7b368cfabf02ca4ec25a9
ILYA ERSHOV <ershov.ilya@gmail.com>
2015-11-16 10:08:27 +0300

    logic scaffolds

--- config.class.php
+++ config.class.php
@@ -9,23 +9,51 @@
  */
 
 header( 'Content-Type: text/plain; charset=utf-8' ) ;
+define('DEBUG' , true) ;
+defined( 'DEBUG') or define('DEBUG' , false) ;
 
-class Foo {
-    public function __toString() {
-        return 'foo';
+class Config {
+    private $data;
+    private $source;
+
+    function __construct($source=NULL, $preload=true){
+        $sourcetype=gettype($source);
+        if(DEBUG) print 'sourcetype: '.$sourcetype.PHP_EOL;
+
+        switch($sourcetype){
+            case "NULL":
+                $this->data=array();
+                break;
+            case "string":
+                $this->source='file';
+                $this->data=array();
+                break;
+            case "array":
+                $this->source='db';
+                $this->data=array();
+                break;
+            case "object":
+                $this->source='db';
+                $this->data=array();
+                break;
+        }
+
     }
 
-    public function __toArray() {
-        return array('foo'=>'bar');
-    }
-    public function __invoke($str) {
-        return "invoke ".$str;
+//    public function __toString() {
+//        return 'foo';
+//    }
+
+    public function __invoke($param, $required=false, $default='') {
+        return "invoke ".$param;
     }
 }
 
-$foo = new Foo;
+try {
+    $config = new Config();
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
 
-var_dump((string)$foo);
-print_r((array)$foo);
-var_dump($foo('test str'));
+var_dump($config('test str'));
 die("Done\n");
